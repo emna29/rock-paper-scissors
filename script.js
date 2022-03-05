6409c528c20705c2b524c74a1f97b17e51646dd0
emna <emna.dridi29@gmail.com>
2022-03-05 16:39:43 +0300

    index.html

---
+++ script.js
@@ -0,0 +1,16 @@
+const computerChoicebisplay = document.getElementByld('computer-choice')
+const userChoiceDisplay = document.getElementById ('user-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
+let userChoice
+
+possibleChoices.forEach(possibleChoice =>possibleChoice.addEventListener('click', (e) => { 
+userChoice = e.target.id
+userChoiceDisplay.innerHTML = userChoice
+generateComputerChoice()
+}))
+
+function generateComputerChoice() {
+const randomNumber = Math.floor(Math.random() * 3) + 1 // or you can use possibleChoices.lenc
+console. log(randomNumber)
+} 
